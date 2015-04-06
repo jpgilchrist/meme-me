d96b03efce1a0d5f8bb85abc38d56614bcb70269
James P. Gilchrist <james.p.gilchrist@gmail.com>
2015-04-06 10:18:52 -0400

    adds collectionView and MemeDetail view

--- MemeMe/Meme.swift
+++ MemeMe/Meme.swift
@@ -2,27 +2,18 @@
 //  Meme.swift
 //  MemeMe
 //
-//  Created by James Gilchrist on 3/31/15.
+//  Created by James Gilchrist on 4/2/15.
 //  Copyright (c) 2015 James Gilchrist. All rights reserved.
 //
 
 import Foundation
-import UIKit
+import CoreData
 
-struct Meme: Printable {
-    var topText: String
-    var bottomText: String
-    var originalImage: UIImage
-    var memedImage: UIImage
-    
-    init(topText:String, bottomText:String, originalImage:UIImage, memedImage:UIImage ) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-    }
-    
-    var description: String {
-        return "top text: \(self.topText) bottom: \(self.bottomText) original image: \(self.originalImage)"
-    }
+class Meme: NSManagedObject {
+
+    @NSManaged var topText: String
+    @NSManaged var bottomText: String
+    @NSManaged var originalImageData: NSData
+    @NSManaged var memedImageData: NSData
+
 }
